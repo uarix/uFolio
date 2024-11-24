c8380d860510900f0cccc893f2e01f4440e5859a
Quarix <144305875+uarix@users.noreply.github.com>
2024-11-24 22:20:31 +0800

    :ambulance: Update base path in index.html and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 import compress from 'vite-plugin-compression';  
 
 export default defineConfig(({ mode }) => ({  
+  base: './',
   server: {  
     host: "::",  
     port: 8080,  
